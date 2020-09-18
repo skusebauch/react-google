f3c2a4c017ecf12324c6c1c70147d5462b4079ff
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-19 01:13:04 +0200

    init react, folder architecture

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>This is amazing</h1>
+
+      {/* Homepage (search page)*/}
+      <Home />
+      {/* Resultpage */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>This is homepage</h1>
+      <div className="home__header">
+        <div className="home__header--left">
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="home__header--right">
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+      <div className="home__body"></div>
+    </div>
+  );
+}
+
+export default Home;
